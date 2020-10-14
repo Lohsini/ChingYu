5a6e7f69275620ece165460c8581c4dfe35e2f2f
Lohi <lohi3152@gmail.com>
2020-10-14 17:10:38 +0800

    add deploy

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,6 @@ const routes = [
     component: Home,
   },
   {
-    path: '/Home',
-    name: 'Home',
-    component: Home,
-  },
-  {
     path: '/about',
     name: 'About',
     component: () => import('../views/About.vue'),
