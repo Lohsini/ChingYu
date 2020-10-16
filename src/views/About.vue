adf0501065482a2bdfffe74d770ec6300b644e66
Lohi <lohi3152@gmail.com>
2020-10-16 15:51:16 +0800

    about and servies page use 92vh content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="about">
-    <h1>about</h1>
+    <div class="container">
+      <div class="test">
+        <h1>關於我們</h1>
+        <h1>開發中</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.about{
+  min-height: 92vh;
+  .container{
+    height: 92vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .test{
+      background-color: rgba(0, 0, 0, 0.08);
+      height: 90%;
+      width: 90%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
+}
 </style>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="service">
-    <h1>service</h1>
+    <div class="container">
+      <div class="test">
+        <h1>服務內容</h1>
+        <h1>開發中</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.service{
+  min-height: 92vh;
+  .container{
+    height: 92vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .test{
+      background-color: rgba(0, 0, 0, 0.08);
+      height: 90%;
+      width: 90%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+  }
+}
 </style>
